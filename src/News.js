ace2963714c7f051fd0a45057bc91a51e68b92b2
Srujan <srujan.deshpande@gmail.com>
2020-07-04 13:27:55 +0530

    Added video and stuff to docs folder

---
+++ src/News.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class News extends Component {
+  render() {
+    return(
+      <iframe width="560"
+      height="315"
+      src="https://www.youtube.com/embed/dQw4w9WgXcQ?controls=0"
+      frameborder="0"
+      allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+      allowfullscreen></iframe>
+    )
+  }
+}
+
+export default News;
